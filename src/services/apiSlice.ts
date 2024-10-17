696c165b4a67b5bc4e56496ce841f2938de1d9d9
Immanuel Diaz <immanuel.diaz@includedhealth.com>
2024-10-16 18:05:01 -0600

    deploy test

--- src/services/apiSlice.ts
+++ src/services/apiSlice.ts
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//const API_SERVICE = "https://service-m2-development.up.railway.app"
+const API_SERVICE = "https://service-m2-production.up.railway.appp"
+
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://service-m2-development.up.railway.app", credentials: 'same-origin' }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_SERVICE, credentials: 'same-origin' }),
   tagTypes: ["User"],
   endpoints: (_) => ({}),
 });
